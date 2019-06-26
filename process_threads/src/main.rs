89d8ee3506caa947c5d9e9636aec7f85c83391fc
Danny Sauer <dsauer@suse.com>
2019-06-26 09:52:55 -0500

    Handle option passing as struct

--- process_threads/src/main.rs
+++ process_threads/src/main.rs
@@ -1,19 +1,28 @@
 use std::process::Command;
 
-fn main() {
-  let cmds = ["/bin/echo",
-    "/bin/echoo",
-    "/bin/date",
-    ];
-
-  t_cmd_launcher(cmds[0], "hello world");
+struct ProcessArgs<'pa> {
+  cmd: &'pa str,
+  args: Option<&'pa str>,
 }
 
-fn t_cmd_launcher(cmd:&str, arg:&str) {
-  let mut kid = Command::new(cmd)
-                        .arg(arg)
-                        .spawn()
-                        .expect(&format!("failed to run '{}'", cmd));
+fn main() {
+  let cmds = [
+    ProcessArgs{ cmd: "/bin/echo", args: Some("hello world") },
+    ProcessArgs{ cmd: "/bin/echoo", args: Some("hello world") },
+    ProcessArgs{ cmd: "/bin/date", args: None },
+    ];
+
+  t_child_launcher(&cmds[0]);
+  t_child_launcher(&cmds[2]);
+}
+
+fn t_child_launcher(cmd_params:&ProcessArgs) {
+  let mut cmd = Command::new(cmd_params.cmd);
+  if cmd_params.args.is_some() {
+    cmd.arg(cmd_params.args.unwrap());
+  }
+  let mut kid = cmd.spawn().expect(&format!("failed to run '{}'", cmd_params.cmd));
+
   let ret = kid.wait()
                .expect("failed to wait on kid");
   assert!(ret.success());
