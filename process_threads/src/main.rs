d829279d7f550a96922c2ad3708ef870ca47708b
Danny Sauer <dsauer@suse.com>
2019-06-26 14:10:27 -0500

    rustfmt

--- process_threads/src/main.rs
+++ process_threads/src/main.rs
@@ -1,29 +1,43 @@
 use std::process::Command;
 
 struct ProcessArgs<'pa> {
-  cmd: &'pa str,
-  args: Option<&'pa str>,
+    cmd: &'pa str,
+    args: Option<&'pa str>,
 }
 
 fn main() {
-  let cmds = [
-    ProcessArgs{ cmd: "/bin/echo", args: Some("hello world") },
-    ProcessArgs{ cmd: "/bin/echoo", args: Some("hello world") },
-    ProcessArgs{ cmd: "/bin/date", args: None },
+    let cmds = [
+        ProcessArgs {
+            cmd: "/bin/echo",
+            args: Some("hello world"),
+        },
+        ProcessArgs {
+            cmd: "/bin/echoo",
+            args: Some("hello world"),
+        },
+        ProcessArgs {
+            cmd: "/bin/date",
+            args: None,
+        },
+        ProcessArgs {
+            cmd: "/bin/sleep",
+            args: inf,
+        },
     ];
 
-  t_child_launcher(&cmds[0]);
-  t_child_launcher(&cmds[2]);
+    t_child_launcher(&cmds[0]);
+    t_child_launcher(&cmds[2]);
 }
 
-fn t_child_launcher(cmd_params:&ProcessArgs) {
-  let mut cmd = Command::new(cmd_params.cmd);
-  if cmd_params.args.is_some() {
-    cmd.arg(cmd_params.args.unwrap());
-  }
-  let mut kid = cmd.spawn().expect(&format!("failed to run '{}'", cmd_params.cmd));
+fn t_child_launcher(cmd_params: &ProcessArgs) {
+    let mut cmd = Command::new(cmd_params.cmd);
+    if cmd_params.args.is_some() {
+        cmd.arg(cmd_params.args.unwrap());
+    }
+    let mut kid = cmd
+        .spawn()
+        .expect(&format!("failed to run '{}'", cmd_params.cmd));
 
-  let ret = kid.wait()
-               .expect("failed to wait on kid");
-  assert!(ret.success());
+    let ret = kid.wait().expect("failed to wait on kid");
+    assert!(ret.success());
 }
